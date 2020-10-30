21321b79413db595728aa6998dbafd28d1597125
spacemonkey1101 <ritambh54@gmail.com>
2020-10-30 23:59:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imageloader/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.imageloader
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
+    }
+}
